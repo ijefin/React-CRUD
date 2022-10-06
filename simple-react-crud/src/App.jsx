1b7c68aee606367369a7061278c066f0759e61cf
ijefin <jeffersonmecmiler@hotmail.com>
2022-10-06 14:27:42 -0300

    using some state method

--- simple-react-crud/src/App.jsx
+++ simple-react-crud/src/App.jsx
@@ -2,10 +2,22 @@ import React from "react";
 
 //creating a class component.
 export default class App extends React.Component {
+  state = {
+    nome: "",
+  };
+
+  //set state para setar o estado passando a propriedade no qual quero alterar
+  changeName = (event) => {
+    this.setState({
+      nome: event.target.value,
+    });
+  };
+
   render() {
     return (
       <>
-        <h1>Olá mundo</h1>
+        <input type="text" value={this.state.nome} onChange={this.changeName} />
+        <h1>Olá mundo {this.state.nome}</h1>
       </>
     );
   }

06d043cd3b267f1153890d18c8af755c69c92c53
ijefin <jeffersonmecmiler@hotmail.com>
2022-10-23 15:53:16 -0300

    some changes in the Home

--- REACT-CRUD/src/views/Home.jsx
+++ REACT-CRUD/src/views/Home.jsx
@@ -3,7 +3,7 @@ import NewProductButton from "../components/Buttons/NewProductButton/NewProductB
 const Home = () => {
   return (
     <>
-      <div className="jumbotron container bg-light">
+      <div className="jumbotron container bg-light pb-5">
         <h1 className="display-3 text-primary pt-5">Bem vindo!</h1>
         <p className="lead">
           Utilize a barra de navegação para acessar as páginas.
